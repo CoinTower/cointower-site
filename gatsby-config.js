72575ecaa02c7529e12005bdc7864b49dcda220b
Nathan Horrigan <nathan_horrigan@icloud.com>
2017-12-29 00:29:54 +0000

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,5 @@ module.exports = {
   siteMetadata: {
     title: `Cointower: Play RPS, Win Money!`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`],
-  pathPrefix: `cointower`
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`]
 }
