5fce9992703ad9eabf4f8dbc95ce3f29f7bbc3ee
Nathan Horrigan <nathan_horrigan@icloud.com>
2018-02-09 12:04:48 +0000

    update

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/Nathan/Sites/Cointower/src/layouts/index.js"
+  import data from "/Users/Nathan/Sites/Cointower/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
