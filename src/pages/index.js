3b2b2d66501c15b418fd229d5fd917dc51ce564f
Nathan Horrigan <nathan_horrigan@icloud.com>
2018-02-09 16:31:37 +0000

    hide video for now, update links

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import CircleLogo from '../images/image-logo.svg'
 import YouTube from 'react-youtube'
 import AppStore from '../images/appstore.svg'
 import GoogPlay from '../images/gplay.svg'
+import Splash from '../images/splash.svg'
 
 const IndexPage = () => (
   <div className="home">
@@ -18,10 +19,14 @@ const IndexPage = () => (
     
       <span className="slogan">PLAY ROCK PAPER SCISSORS, WIN REAL CASH.</span>
 
-      <div className="youtube-container">
+      {/* <div className="youtube-container">
         <YouTube
           videoId={'QEkC_Q1ERyk'}
           className="youtube" />
+      </div> */}
+
+      <div className="backup-splash">
+        <img className="splash-img" src={Splash} alt="splash" />
       </div>
 
       <div className="app-stores">
@@ -31,8 +36,16 @@ const IndexPage = () => (
     </div>
 
     <div className="footer">
-      <span>Copyright Spanky & Co. 2017</span>
-      <a className="tc-link" href="#"><span>Terms and Conditions</span></a>
+      <span className="copy">Copyright Spanky & Co. 2017</span>
+      <span className="tc-links">
+        <a className="tc-link" href="https://firebasestorage.googleapis.com/v0/b/cointower-4502d.appspot.com/o/legal%2FTerms-of-Service.pdf?alt=media&token=8e1115da-5e00-4507-964c-0f326f2b959d">
+          Terms
+        </a>
+        <span> and </span>
+        <a className="tc-link" href="https://firebasestorage.googleapis.com/v0/b/cointower-4502d.appspot.com/o/legal%2FPrivacy-Policy.pdf?alt=media&token=03b8e906-703e-4626-af86-3246d184d328">
+          Privacy
+        </a>
+      </span>
     </div>
   
   </div>
